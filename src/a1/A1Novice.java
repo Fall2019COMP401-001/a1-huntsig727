b4f5727db663330a6aa7a85959ea0028b32ea9ae
huntersigmund <huntersigmund@hunters-mbp-2.wireless-1x.unc.edu>
2019-08-30 14:07:42 -0400

    first attempt!

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,40 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int totalCustomers = scan.nextInt();
+		
+		for (int i = 0; i <= totalCustomers; i++) {
+			String[] names;
+			names[0] = scan.next();
+			names[1] = scan.next();
+			
+			int items = scan.nextInt();
+			
+			int[] quantityArray;
+			String[] itemNames;		
+			double[] priceArray;
+			
+			for (int j = 0; j <= items; j++) {
+				
+				quantityArray[j] = scan.nextInt();
+				itemNames[j] = scan.next();
+				priceArray[j] = scan.nextDouble();
+			}
+			
+			double total = totalPrice(quantityArray, priceArray);
+			System.out.println(names[0].charAt(0) + ". " + names[1] + ": " + String.format("%.2f, total"))
+			
+		}
+		
+	}
+	
+	public static double totalPrice(int[] a, double[] b) {
+		double totalCost = 0;
+		for (int i = 0; i <= a.length; i++) {
+			totalCost += a[i] * b[i];
+		}
+		
+		return totalCost;
 	}
 }
