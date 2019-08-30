c37eee329b9e0b0910844e54c362e82a2218d7de
huntersigmund <huntersigmund@hunters-mbp-2.wireless-1x.unc.edu>
2019-08-30 14:32:08 -0400

    changed loop equality

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,7 +10,7 @@ public class A1Novice {
 		
 		int totalCustomers = scan.nextInt();
 		
-		for (int i = 0; i <= totalCustomers; i++) {
+		for (int i = 0; i < totalCustomers; i++) {
 			String[] names = new String[2];
 			names[0] = scan.next();
 			names[1] = scan.next();
@@ -21,14 +21,16 @@ public class A1Novice {
 			String[] itemNames = new String[items];		
 			double[] priceArray = new double[items];
 			
-			for (int j = 0; j <= items; j++) {
+			for (int j = 0; j < items; j++) {
 				
 				quantityArray[j] = scan.nextInt();
+			//	System.out.println(quantityArray[j]);
 				itemNames[j] = scan.next();
 				priceArray[j] = scan.nextDouble();
 			}
 			
 			double total = totalPrice(quantityArray, priceArray);
+			// System.out.println(names[0].charAt(1));
 			System.out.println(names[0].charAt(0) + ". " + names[1] + ": " + String.format("%.2f", total));
 			
 		}
@@ -39,7 +41,7 @@ public class A1Novice {
 	
 	public static double totalPrice(int[] a, double[] b) {
 		double totalCost = 0;
-		for (int i = 0; i <= a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			totalCost += a[i] * b[i];
 		}
 		
