ad1f63910c0f559d72423ddd320fb3305892fc78
huntersigmund <huntersigmund@hunters-mbp-2.wireless-1x.unc.edu>
2019-08-31 20:10:48 -0400

    first try!

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,89 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int totalItems = scan.nextInt();
+		
+		String[] foodNames = new String[totalItems];
+		double[] foodPrices = new double[totalItems];
+		
+		for (int i = 0; i < totalItems; i++) {
+			foodNames[i] = scan.next();
+			foodPrices[i] = scan.nextDouble();
+		}
+		
+		int totalCustomers = scan.nextInt();
+		
+		String[] customerNames = new String[totalCustomers];
+		int[] numberOfItems = new int[totalCustomers];
+		
+		int[] amountsOfEachFood = new int[foodNames.length];
+		int[] numberOfPplEachFood = new int[foodNames.length];
+		
+		for (int i = 0; i < totalCustomers; i++) {
+			customerNames[i] = scan.next() + scan.next();
+			numberOfItems[i] = scan.nextInt();
+			
+			int[] numberOfEach = new int[numberOfItems[i]];
+			String[] nameOfItems = new String[numberOfItems[i]];
+				
+			for (int j = 0; j < numberOfItems[i]; j++) {
+					numberOfEach[j] = scan.nextInt();
+					nameOfItems[j] = scan.next();
+					
+			}	
+			
+			for (int y = 0; y < nameOfItems.length; y++) {
+				for (int w = 0; w < foodNames.length; w++) {
+					if (foodNames[w].equals(nameOfItems[y])) {
+						amountsOfEachFood[w] += numberOfEach[y];
+						numberOfPplEachFood[w] += 1;
+					}
+				}
+		}				
+		}
+		for (int i = 0; i < amountsOfEachFood.length; i++) {
+			if (amountsOfEachFood[i] == 0) {
+				System.out.println("No customers bought " + foodNames[i]);
+			} else {
+				System.out.println(numberOfPplEachFood[i] + " customers bought " + amountsOfEachFood[i] + " " + foodNames[i]);
+			}
+		}
 	}
 }
+					
+					
+					
+//			for (int z = 0; z < foodNames.length; z++) {
+//				
+//					
+//				total = totalNumberOfPeople(foodNames[z], nameOfItems);
+//				numberOfFoods = totalNumberOfItems(foodNames[z], numberOfEach, nameOfItems);
+//							
+//				
+//			if (total == 0) {
+//				System.out.println("No customers bought " + foodNames[z]);
+//			} else {
+//				System.out.println(total + " customers bought " + numberOfFoods + " " + foodNames[z]);
+//			}
+//			}	
+//		}
+	
+//	public static int totalNumberOfItems(String a, int[] b, String[] c) {
+//		int x = 0;
+//		for (int i = 0; i < b.length; i++) {
+//			if (a.equals(c[i])){
+//				x += b[i];
+//			}
+//		}
+//		return x;
+//	}
+//	public static int totalNumberOfPeople(String a, String[] c) {
+//		int y = 0;
+//		for (int i = 0; i < c.length; i++) {
+//			if (a.equals(c[i])) {
+//				y += 1;
+//			}
+//		}
+//		return y;
+//	}
+//}
